28bee421d109afe1310ba98cd770c9f77359b1a9
dannycaihaoming <dannycaihaoming@gmail.com>
2017-10-24 14:21:32 +0800

    VideoBackground

--- VideoBackgound/VideoBackgound/ViewController.swift
+++ VideoBackgound/VideoBackgound/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: AVPlayerViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        let filePath = Bundle.main.path(forResource: "moments", ofType: "mp4")
+        let sourceUrl = NSURL.fileURL(withPath: filePath!)
+        
+//AVPlayerController
+        let player = AVPlayer.init(url: sourceUrl)
+        
+        self.player = player
+        
+        self.showsPlaybackControls = false
+        
+        self.player?.play()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.playerItemDidReachEnd), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player?.currentItem)
+        
+        
+//AVPlayer
+
+//
+//        let movieAsset = AVURLAsset.init(url: sourceUrl)
+//        
+//        let playerItem = AVPlayerItem.init(asset: movieAsset)
+//        
+//        let player = AVPlayer.init(playerItem: playerItem)
+//        
+//        let playerLayer = AVPlayerLayer.init(player: player)
+//        
+//        playerLayer.frame = self.view.bounds
+//        
+//        self.view.layer.addSublayer(playerLayer)
+//        
+//        player.play()
+    }
+    
+    
+    func playerItemDidReachEnd(){
+        self.player?.seek(to: kCMTimeZero)
+        self.player?.play()
+    
     }
 
     override func didReceiveMemoryWarning() {
